945c956081d182852dd97a6be11dc0d91d060d19
jrshor3 <112657230+jrshor3@users.noreply.github.com>
2022-10-27 15:34:54 -0400

    Update ReportForm.java
    Added first text to ReportForm

--- src/main/ReportForm.java
+++ src/main/ReportForm.java
@@ -5,6 +5,7 @@ import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.text.Text;
 import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 
@@ -15,22 +16,21 @@ public class ReportForm extends Application {
     }
 	
 	 @Override
-	    public void start(Stage primaryStage) {
-	        primaryStage.setTitle("Hello World!");
-	        Button btn = new Button();
+	    public void start(Stage form) {
+	        form.setTitle("Furman University Police Use of Force Report");
+	        Text btn = new Text();
 	        btn.setText("Say 'Hello World'");
-	        btn.setOnAction(new EventHandler<ActionEvent>() {
-	 
-	            @Override
-	            public void handle(ActionEvent event) {
-	                System.out.println("Hello World!");
-	            }
-	        });
+	        //btn.setOnAction(new EventHandler<ActionEvent>() {
+	        //    	@Override
+	        //    	public void handle(ActionEvent event) {
+	        //        	System.out.println("Hello World!");
+	        //    	}
+	        //	});
 	        
 	        StackPane root = new StackPane();
 	        root.getChildren().add(btn);
-	        primaryStage.setScene(new Scene(root, 300, 250));
-	        primaryStage.show();
+	        form.setScene(new Scene(root, 300, 250));
+	        form.show();
 	    }
 	
 }
