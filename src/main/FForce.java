2577fd5b177f7e6bc81d9aadef54607e893125e1
Luke Kvamme <Luke.govols@me.com>
2022-10-27 13:50:32 -0400

    stuff
    stuff descriptioj

--- src/main/FForce.java
+++ src/main/FForce.java
@@ -1,14 +1,36 @@
 package main;
 
-public class FForce {
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.geometry.Insets;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.GridPane;
+import javafx.scene.text.Font;
+import javafx.scene.text.FontWeight;
+import javafx.scene.text.Text;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class FForce extends Application {
+	private static Scene scene;
 
 	public FForce() {
-		
-	}
-	
-	public static void main(String[] args)
-	{
-		System.out.println("hello");
+
 	}
 
+	public void start(Stage stage) throws Exception {
+		Parent root = FXMLLoader.load(getClass().getResource("primary.fxml"));
+
+		Scene scene = new Scene(root, 600, 600);
+
+		stage.setTitle("FXML Welcome");
+		stage.setScene(scene);
+		stage.show();
+	}
+	
+	
 }
