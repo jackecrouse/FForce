46d13dd0586915f1caeee29b14c8e67d899bc0d2
jackecrouse <112016157+jackecrouse@users.noreply.github.com>
2022-10-27 14:19:50 -0400

    added classpath to gitignore

--- src/main/FForce.java
+++ src/main/FForce.java
@@ -7,7 +7,8 @@ import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
- 
+
+
 public class FForce extends Application {
     public static void main(String[] args) {
         launch(args);
