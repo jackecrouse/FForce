d82a4d8e3127d2a9b8eb1f249a03bd99101bea6f
Luke Kvamme <Luke.govols@me.com>
2022-10-27 14:16:07 -0400

    FXML not working
    help

--- src/main/FForce.java
+++ src/main/FForce.java
@@ -7,12 +7,14 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.GridPane;
 import javafx.scene.text.Font;
 import javafx.scene.text.FontWeight;
 import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
+import java.io.FileInputStream;
 import java.io.IOException;
 
 public class FForce extends Application {
@@ -23,8 +25,10 @@ public class FForce extends Application {
 	}
 
 	public void start(Stage stage) throws Exception {
-		Parent root = FXMLLoader.load(getClass().getResource("primary.fxml"));
-
+//		FileInputStream fileInputStream = new FileInputStream(new File("src/main/java/CRUD/bnkseekCRUD.fxml"));
+ // 		Parent root = FXMLLoader.load(getClass().getResource("/Users/lukekvamme/git/FForce/src/main/primary.fxml"));
+//		Parent root2 = FXMLLoader.setController("primary.fxml");
+		AnchorPane root = (AnchorPane) FXMLLoader.load(FForce.class.getResource("primary.fxml"));
 		Scene scene = new Scene(root, 600, 600);
 
 		stage.setTitle("FXML Welcome");
