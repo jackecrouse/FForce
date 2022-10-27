a8990656880d2ea65f624295d12da36a1bf824a1
jackecrouse <112016157+jackecrouse@users.noreply.github.com>
2022-10-27 16:27:59 -0400

    login

--- src/main/FForce.java
+++ src/main/FForce.java
@@ -5,6 +5,10 @@ import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.HBox;
 import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 
@@ -16,20 +20,36 @@ public class FForce extends Application {
     
     @Override
     public void start(Stage primaryStage) {
-        primaryStage.setTitle("Hello World!");
-        Button btn = new Button();
-        btn.setText("Say 'Hello World'");
-        btn.setOnAction(new EventHandler<ActionEvent>() {
- 
+    	
+        primaryStage.setTitle("Login");
+        Button btn = new Button("Submit");
+        btn.setTranslateY(40);
+
+        TextField user = new TextField();
+        user.setTranslateY(-50);
+        
+        PasswordField pass = new PasswordField();
+        pass.setTranslateY(-10);
+        
+        
+        btn.setOnAction(new EventHandler<ActionEvent>() 
+        {
             @Override
             public void handle(ActionEvent event) {
-                System.out.println("Hello World!");
+                System.out.println(user.getText());
+                System.out.println(pass.getText());
+                
             }
         });
         
         StackPane root = new StackPane();
-        root.getChildren().add(btn);
+        
+        root.getChildren().addAll(btn, user, pass);
+
+        
         primaryStage.setScene(new Scene(root, 300, 250));
+
+
         primaryStage.show();
     }
 }
