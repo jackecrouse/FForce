3f1accb1710201aa406671bd3123ddc6c472dab8
jrshor3 <112657230+jrshor3@users.noreply.github.com>
2022-11-02 10:41:56 -0400

    fixed run() to create()

--- src/main/UOFFormDriver.java
+++ src/main/UOFFormDriver.java
@@ -3,8 +3,7 @@ package main;
 public class UOFFormDriver {
 	
 	public static void main(String[] args) {
-		UOFSubjectForm a = new UOFSubjectForm();
-		a.run()
-		;
+		UOFIncidentForm mainForm = new UOFIncidentForm();
+		mainForm.create();
 	}
 }
