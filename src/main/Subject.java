8b1abe852a8e9114599222d8b14cfb58cc933083
jrshor3 <112657230+jrshor3@users.noreply.github.com>
2022-11-02 10:37:50 -0400

    created Subject.java
    contains subject information pertaining to an incident

---
+++ src/main/Subject.java
@@ -0,0 +1,26 @@
+package main;
+
+import java.util.Date;
+import java.util.Vector;
+
+public class Subject {
+	
+	public String firstName;
+	public String middleName;
+	public String lastName;
+	public String sex;
+	public String race;
+	public Date dateOfBirth;
+	public boolean wasInjured;
+	public boolean wasKilled;
+	public boolean hadWeapon;
+	public boolean wasArrested;
+	public boolean hadMedicalTreatment;
+	public String injuries;
+	public String charges;
+	public Vector<String> influenceType;
+	public Vector<String> actions;
+	public Vector<String> uofAgainst;
+	public int shotsFiredAt;	
+	
+}
