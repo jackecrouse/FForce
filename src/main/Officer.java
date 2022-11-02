cd65a14699905f919113dcf58323203cf41854eb
jrshor3 <112657230+jrshor3@users.noreply.github.com>
2022-11-02 10:37:29 -0400

    created Officer.java
    contains officer information pertaining to an incident

---
+++ src/main/Officer.java
@@ -0,0 +1,24 @@
+package main;
+
+import java.util.Date;
+
+public class Officer {
+
+	public int badgeNumber;
+	public String firstName;
+	public String middleName;
+	public String lastName;
+	public String sex;
+	public String race;
+	public Date dateOfBirth;
+	public Date serviceStart;
+	public String rank;
+	public String duty;
+	public boolean wasInjured;
+	public boolean wasKilled;
+	public boolean wasOnDuty;
+	public boolean wasUniformed;
+	public boolean hadMedicalTreatment;
+	public String injuries;
+	
+}
