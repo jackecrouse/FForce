1db1fe766af1c4ad0f9aecb487c399bb4e973838
jrshor3 <112657230+jrshor3@users.noreply.github.com>
2022-11-02 10:36:52 -0400

    created Incident.java
    contains pure incident information

---
+++ src/main/Incident.java
@@ -0,0 +1,15 @@
+package main;
+
+import java.util.Date;
+import java.util.Vector;
+
+public class Incident {
+	
+	
+	public Officer officer;
+	public Vector<Subject> subjects;
+	public Date incidentDate;
+	public String location;
+	public String type;
+	
+}
