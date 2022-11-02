20895a4183cc80ef66242c6c5980f9abd00c4dae
jrshor3 <112657230+jrshor3@users.noreply.github.com>
2022-11-02 09:46:00 -0400

    added main to UOFFormDriver

--- src/main/UOFFormDriver.java
+++ src/main/UOFFormDriver.java
@@ -1,6 +1,10 @@
 package main;
 
 public class UOFFormDriver {
-	UOFSubjectForm a;
-	a.run();
+	
+	public static void main(String[] args) {
+		UOFSubjectForm a = new UOFSubjectForm();
+		a.run()
+		;
+	}
 }
