9304062eab3dd5f140015d4b79e04a9566607790
jrshor3 <112657230+jrshor3@users.noreply.github.com>
2022-11-03 16:20:33 -0400

    Placed initial states in initializer for subject form

--- src/form/UOFSubjectForm.java
+++ src/form/UOFSubjectForm.java
@@ -105,227 +105,184 @@ public class UOFSubjectForm extends Application {
 		hbxSubject = new HBox();
 		pneSubject.getChildren().add(hbxSubject);
 		
+		// everything on the left half of the screen start
 		vbxSubjectInfo = new VBox();
 		hbxSubject.getChildren().add(vbxSubjectInfo);
 		
-		txtSubjectInfo = new Text();
-		txtSubjectInfo.setText("Subject Information");
+		txtSubjectInfo = new Text("Subject Information");
 		vbxSubjectInfo.getChildren().add(txtSubjectInfo);
 		
+		// Subject's personal information start
 		grdSubjectInfo = new GridPane();
 		vbxSubjectInfo.getChildren().add(grdSubjectInfo);
 		
-		lblFirstName = new Label();
-		lblFirstName.setText("First Name");
+		lblFirstName = new Label("First Name");
 		grdSubjectInfo.add(lblFirstName, 0, 0);
-		txfFirstName = new TextField();
-		txfFirstName.setText(subject.firstName);
+		txfFirstName = new TextField(subject.firstName);
 		grdSubjectInfo.add(txfFirstName, 1, 0);
 		
-		lblMiddleName = new Label();
-		lblMiddleName.setText("Middle Name");
+		lblMiddleName = new Label("Middle Name");
 		grdSubjectInfo.add(lblMiddleName, 0, 1);
-		txfMiddleName = new TextField();
-		txfMiddleName.setText(subject.middleName);
+		txfMiddleName = new TextField(subject.middleName);
 		grdSubjectInfo.add(txfMiddleName, 1, 1);
 
-		lblLastName = new Label();
-		lblLastName.setText("Last Name");
+		lblLastName = new Label("Last Name");
 		grdSubjectInfo.add(lblLastName, 0, 2);
-		txfLastName = new TextField();
-		txfLastName.setText(subject.lastName);
+		txfLastName = new TextField(subject.lastName);
 		grdSubjectInfo.add(txfLastName, 1, 2);
 
-		lblSex = new Label();
-		lblSex.setText("Sex");
+		lblSex = new Label("Sex");
 		grdSubjectInfo.add(lblSex, 0, 3);
-		txfSex = new TextField();
-		txfSex.setText(subject.sex);
+		txfSex = new TextField(subject.sex);
 		grdSubjectInfo.add(txfSex, 1, 3);
 
-		lblRace = new Label();
-		lblRace.setText("Race");
+		lblRace = new Label("Race");
 		grdSubjectInfo.add(lblRace, 0, 4);
-		txfRace = new TextField();
-		txfRace.setText(subject.race);
+		txfRace = new TextField(subject.race);
 		grdSubjectInfo.add(txfRace, 1, 4);
 		
-		lblDOB = new Label();
-		lblDOB.setText("Date of Birth");
+		lblDOB = new Label("Date of Birth");
 		grdSubjectInfo.add(lblDOB, 0, 5);
-		dtpDOB = new DatePicker();
-		dtpDOB.setValue(subject.dateOfBirth.toInstant().atZone(ZoneId.systemDefault()).toLocalDate());
+		dtpDOB = new DatePicker(subject.dateOfBirth.toInstant().atZone(ZoneId.systemDefault()).toLocalDate());
 		grdSubjectInfo.add(dtpDOB, 1, 5);
+		// Subject's personal information end
 		
-		cbxSubjectInjured = new CheckBox();
-		cbxSubjectInjured.setText("Subject was Injured");
+		cbxSubjectInjured = new CheckBox("Subject was Injured");
 		cbxSubjectInjured.setSelected(subject.wasInjured);
 		vbxSubjectInfo.getChildren().add(cbxSubjectInjured);
 		
-		cbxSubjectKilled = new CheckBox();
-		cbxSubjectKilled.setText("Subject was Killed");
+		cbxSubjectKilled = new CheckBox("Subject was Killed");
 		cbxSubjectKilled.setSelected(subject.wasKilled);
 		vbxSubjectInfo.getChildren().add(cbxSubjectKilled);
 		
-		cbxSubjectWeaponed = new CheckBox();
-		cbxSubjectWeaponed.setText("Subject had a Weapon");
+		cbxSubjectWeaponed = new CheckBox("Subject had a Weapon");
 		cbxSubjectWeaponed.setSelected(subject.wasWeaponed);
 		vbxSubjectInfo.getChildren().add(cbxSubjectWeaponed);
 		
-		cbxSubjectArrested = new CheckBox();
-		cbxSubjectArrested.setText("Subject Was Arrested");
+		cbxSubjectArrested = new CheckBox("Subject Was Arrested");
 		cbxSubjectArrested.setSelected(subject.wasArrested);
 		vbxSubjectInfo.getChildren().add(cbxSubjectArrested);
 		
-		lblInjuries = new Label();
-		lblInjuries.setText("Describe Injuries to Subject");
+		lblInjuries = new Label("Describe Injuries to Subject");
 		vbxSubjectInfo.getChildren().add(lblInjuries);
-		txaInjuries = new TextArea();
-		txaInjuries.setPrefHeight(80.0);
-		txaInjuries.setPrefWidth(100.0);
-		txaInjuries.setText(subject.injuries);
+		txaInjuries = new TextArea(subject.injuries);
+		txaInjuries.setPrefSize(100.0, 80.0);
 		vbxSubjectInfo.getChildren().add(txaInjuries);
 		
-		lblInfluence = new Label();
-		lblInfluence.setText("Under the Influence of:");
+		lblInfluence = new Label("Under the Influence of:");
 		vbxSubjectInfo.getChildren().add(lblInfluence);
 		
-		cbxDrugs = new CheckBox();
-		cbxDrugs.setText("Drugs");
+		cbxDrugs = new CheckBox("Drugs");
 		cbxDrugs.setSelected(subject.influence.contains(cbxDrugs.getText()));
 		vbxSubjectInfo.getChildren().add(cbxDrugs);
 		
-		cbxAlcohol = new CheckBox();
-		cbxAlcohol.setText("Alcohol");
+		cbxAlcohol = new CheckBox("Alcohol");
 		cbxAlcohol.setSelected(subject.influence.contains(cbxAlcohol.getText()));
 		vbxSubjectInfo.getChildren().add(cbxAlcohol);
 		
-		cbxMentalIllness = new CheckBox();
-		cbxMentalIllness.setText("Mental Illness");
+		cbxMentalIllness = new CheckBox("Mental Illness");
 		cbxMentalIllness.setSelected(subject.influence.contains(cbxMentalIllness.getText()));
 		vbxSubjectInfo.getChildren().add(cbxMentalIllness);
 		
-		cbxOtherCondition = new CheckBox();
-		cbxOtherCondition.setText("Other Condition");
+		cbxOtherCondition = new CheckBox("Other Condition");
 		cbxOtherCondition.setSelected(subject.influence.contains(cbxOtherCondition.getText()));
 		vbxSubjectInfo.getChildren().add(cbxOtherCondition);
 		
-		txfOtherCondition = new TextField();
-		txfOtherCondition.setText(subject.otherInfluence);
+		txfOtherCondition = new TextField(subject.otherInfluence);
 		vbxSubjectInfo.getChildren().add(txfOtherCondition);
 		
-		lblCharges = new Label();
-		lblCharges.setText("Charges");
+		lblCharges = new Label("Charges");
 		vbxSubjectInfo.getChildren().add(lblCharges);
-		txaCharges = new TextArea();
-		txaCharges.setPrefHeight(80.0);
-		txaCharges.setPrefWidth(100.0);
-		txaCharges.setText(subject.charges);
+		txaCharges = new TextArea(subject.charges);
+		txaCharges.setPrefSize(100.0,80.0);
 		vbxSubjectInfo.getChildren().add(txaCharges);
+		// everything on the left half of the screen end
 		
-		sprInformationActions = new Separator();
-		sprInformationActions.setOrientation(Orientation.VERTICAL);
+		sprInformationActions = new Separator(Orientation.VERTICAL);
 		hbxSubject.getChildren().add(sprInformationActions);
 		
+		// everything on the right half of the screen start
 		vbxActions = new VBox();
-		hbxSubject.getChildren().add(vbxActions);
+		hbxSubject.getChildren().add(vbxActions); 
 		
-		txtSubjectActions = new Text();
-		txtSubjectActions.setText("Subject's Actions");
+		// Subject's action start
+		txtSubjectActions = new Text("Subject's Actions");
 		vbxActions.getChildren().add(txtSubjectActions);
 		
-		cbxResisted = new CheckBox();
-		cbxResisted.setText("Resisted Police Officer Control/Arrest");
+		cbxResisted = new CheckBox("Resisted Police Officer Control/Arrest");
 		cbxResisted.setSelected(subject.actions.contains(cbxResisted.getText()));
 		vbxActions.getChildren().add(cbxResisted);
 		
-		cbxThreatOrAttack = new CheckBox();
-		cbxThreatOrAttack.setText("Physical Treat/Attack on Officer or Another");
+		cbxThreatOrAttack = new CheckBox("Physical Treat/Attack on Officer or Another");
 		cbxThreatOrAttack.setSelected(subject.actions.contains(cbxThreatOrAttack.getText()));
 		vbxActions.getChildren().add(cbxThreatOrAttack);
 		
-		cbxKnife = new CheckBox();
-		cbxKnife.setText("Threatened/Attacked Officer or Another With a Knife/Weapon");
+		cbxKnife = new CheckBox("Threatened/Attacked Officer or Another With a Knife/Weapon");
 		cbxKnife.setSelected(subject.actions.contains(cbxKnife.getText()));
 		vbxActions.getChildren().add(cbxKnife);
 		
-		cbxMotorVehicle = new CheckBox();
-		cbxMotorVehicle.setText("Threatened/Attacked Officer or Another with Motor Vehicle");
+		cbxMotorVehicle = new CheckBox("Threatened/Attacked Officer or Another with Motor Vehicle");
 		cbxMotorVehicle.setSelected(subject.actions.contains(cbxMotorVehicle.getText()));
 		vbxActions.getChildren().add(cbxMotorVehicle);
 		
-		cbxFirearm = new CheckBox();
-		cbxFirearm.setText("Threatened Officer or Another With a Firearm");
+		cbxFirearm = new CheckBox("Threatened Officer or Another With a Firearm");
 		cbxFirearm.setSelected(subject.actions.contains(cbxFirearm.getText()));
 		vbxActions.getChildren().add(cbxFirearm);
 		
-		cbxShotFirearm = new CheckBox();
-		cbxShotFirearm.setText("Fired at Officer or Another");
+		cbxShotFirearm = new CheckBox("Fired at Officer or Another");
 		cbxShotFirearm.setSelected(subject.actions.contains(cbxShotFirearm.getText()));
 		vbxActions.getChildren().add(cbxShotFirearm);
 		
-		cbxOtherAction = new CheckBox();
-		cbxOtherAction.setText("Other (Specify)");
+		cbxOtherAction = new CheckBox("Other (Specify)");
 		cbxOtherAction.setSelected(subject.actions.contains(cbxOtherAction.getText()));
 		vbxActions.getChildren().add(cbxOtherAction);
 		
-		txfOtherAction = new TextField();
-		txfOtherAction.setText(subject.otherActions);
+		txfOtherAction = new TextField(subject.otherActions);
 		vbxActions.getChildren().add(txfOtherAction);
+		// Subject's actions end
 		
 		sprActionsUOF = new Separator();
-		sprActionsUOF.setPrefWidth(200.0);
 		vbxActions.getChildren().add(sprActionsUOF);
 		
-		txtUOF = new Text();
-		txtUOF.setText("Officer's Use of Force Toward Subject");
+		// Officer's use of force start
+		txtUOF = new Text("Officer's Use of Force Toward Subject");
 		vbxActions.getChildren().add(txtUOF);
 		
-		cbxComplianceHold = new CheckBox();
-		cbxComplianceHold.setText("Compliance Hold");
+		cbxComplianceHold = new CheckBox("Compliance Hold");
 		cbxComplianceHold.setSelected(subject.uofAgainst.contains(cbxComplianceHold.getText()));
 		vbxActions.getChildren().add(cbxComplianceHold);
 		
-		cbxHandsFistFeet = new CheckBox();
-		cbxHandsFistFeet.setText("Hands/Fist/Feet");
+		cbxHandsFistFeet = new CheckBox("Hands/Fist/Feet");
 		cbxHandsFistFeet.setSelected(subject.uofAgainst.contains(cbxHandsFistFeet.getText()));
 		vbxActions.getChildren().add(cbxHandsFistFeet);
 		
-		cbxElectric = new CheckBox();
-		cbxElectric.setText("Electronic Control Device");
+		cbxElectric = new CheckBox("Electronic Control Device");
 		cbxElectric.setSelected(subject.uofAgainst.contains(cbxElectric.getText()));
 		vbxActions.getChildren().add(cbxElectric);
 		
-		cbxChemical = new CheckBox();
-		cbxChemical.setText("Chemical Agent");
+		cbxChemical = new CheckBox("Chemical Agent");
 		cbxChemical.setSelected(subject.uofAgainst.contains(cbxChemical.getText()));
 		vbxActions.getChildren().add(cbxChemical);
 		
-		cbxBaton = new CheckBox();
-		cbxBaton.setText("Strike/Use Baton or Other Object");
+		cbxBaton = new CheckBox("Strike/Use Baton or Other Object");
 		cbxBaton.setSelected(subject.uofAgainst.contains(cbxBaton.getText()));
 		vbxActions.getChildren().add(cbxBaton);
 		
-		cbxOtherUOF = new CheckBox();
-		cbxOtherUOF.setText("Other (Specify)");
+		cbxOtherUOF = new CheckBox("Other (Specify)");
 		cbxOtherUOF.setSelected(subject.uofAgainst.contains(cbxOtherUOF.getText()));
 		vbxActions.getChildren().add(cbxOtherUOF);
 		
-		txfOtherUOF = new TextField();
-		txfOtherUOF.setText(subject.otherUOF);
+		txfOtherUOF = new TextField(subject.otherUOF);
 		vbxActions.getChildren().add(txfOtherUOF);
 		
-		lblFirearm = new Label();
-		lblFirearm.setText("Firearm:");
+		lblFirearm = new Label("Firearm:");
 		vbxActions.getChildren().add(lblFirearm);
 		
-		cbxFirearmAimed = new CheckBox();
-		cbxFirearmAimed.setText("Aimed");
+		cbxFirearmAimed = new CheckBox("Aimed");
 		cbxFirearmAimed.setSelected(subject.uofAgainst.contains(cbxFirearmAimed.getText()));
 		vbxActions.getChildren().add(cbxFirearmAimed);
 		
-		cbxFirearmDischarged = new CheckBox();
-		cbxFirearmDischarged.setText("Discharged");
+		cbxFirearmDischarged = new CheckBox("Discharged");
 		cbxFirearmDischarged.setSelected(subject.uofAgainst.contains(cbxFirearmDischarged.getText()));
 		vbxActions.getChildren().add(cbxFirearmDischarged);
 		
@@ -333,13 +290,12 @@ public class UOFSubjectForm extends Application {
 		hbxNumberOfShots.setAlignment(Pos.CENTER_LEFT);
 		vbxActions.getChildren().add(hbxNumberOfShots);
 		
-		lblNumberOfShots = new Label();
-		lblNumberOfShots.setText("Number of Shots Fired");
+		lblNumberOfShots = new Label("Number of Shots Fired");
 		hbxNumberOfShots.getChildren().add(lblNumberOfShots);
 		
-		txfNumberOfShots = new TextField();
-		txfNumberOfShots.setText("" + subject.numberOfShots);
+		txfNumberOfShots = new TextField("" + subject.numberOfShots);
 		hbxNumberOfShots.getChildren().add(txfNumberOfShots);
+		// Officer's use of force end
 		
 		sprUOFSubmit = new Separator();
 		vbxActions.getChildren().add(sprUOFSubmit);
@@ -347,8 +303,7 @@ public class UOFSubjectForm extends Application {
 		spnSubmit = new StackPane();
 		vbxActions.getChildren().add(spnSubmit);
 		
-		btnSubmit = new Button();
-		btnSubmit.setText("Save and Submit");
+		btnSubmit = new Button("Save and Submit");
 		btnSubmit.setOnAction(new EventHandler<ActionEvent>() {
             @Override
             public void handle(ActionEvent event) {
